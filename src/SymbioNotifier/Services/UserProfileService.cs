a67356c8936035b21e910af8f409c340ec365986
Jochen Linnemann <jochen.linnemann@p-und-z.de>
2018-03-07 13:42:20 +0100

    Add example project source code

---
+++ src/SymbioNotifier/Services/UserProfileService.cs
@@ -0,0 +1,35 @@
+﻿using SymbioNotifier.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SymbioNotifier.Services
+{
+    public interface IUserProfileService
+    {
+        IEnumerable<User> AllUsers { get; }
+        User GetUser(string upn);
+        void SaveChanges();
+    }
+
+    public class MemoryUserProfileService : IUserProfileService
+    {
+        private readonly Dictionary<string, User> _users = new Dictionary<string, User>();
+
+        public IEnumerable<User> AllUsers => _users.Values;
+
+        public User GetUser(string upn)
+        {
+            if (!_users.ContainsKey(upn))
+                _users[upn] = new User();
+
+            return _users[upn];
+        }
+
+        public void SaveChanges()
+        {
+            // only needed in persisting services
+        }
+    }
+}
